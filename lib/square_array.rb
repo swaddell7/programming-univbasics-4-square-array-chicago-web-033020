d74077e53391344dd9b6d6ca7c109dfcfaca6c76
Stefani Waddell <stefani.waddell@hotmail.com>
2020-03-06 17:37:04 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,14 @@
 def square_array(array)
   
   counter = 0 
+  new_array = []
   
   while array[counter] do 
     array[counter]**2 
     counter += 1
+    
+    new_array.push
+    
   end 
   
 end
